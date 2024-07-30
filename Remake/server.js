4d0b32f059eebde3d2daa465acc8e9df1b654ea6
kratae-Watcharaporn <138270421+Watcharaporn-Aiamlamai@users.noreply.github.com>
2024-07-30 20:49:59 +0700

    no message

--- Remake/server.js
+++ Remake/server.js
@@ -52,6 +52,10 @@ app.post('/api/pencil', async (req, res) => {
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
