929d1304b22e116ed9c7cbbc52df02340f9b1bbd
kratae-Watcharaporn <138270421+Watcharaporn-Aiamlamai@users.noreply.github.com>
2024-08-16 19:42:45 +0700

    no message

--- Remake/index.js
+++ Remake/index.js
@@ -242,7 +242,7 @@ function sendDataToServer(numTouches) {
   console.log('Number of touches:', numTouches, currentPageName,user );
   
   // Replace the URL with the server endpoint where you want to send the data
-  fetch('https://k0c9lchx-3000.asse.devtunnels.ms/api/pencil', {
+  fetch('http://localhost:3000/api/pencil', {
     method: 'POST',
     mode: 'cors',
     headers: {
