0171fb7ae98c15aa639e53c25a402cc7c8f49129
kratae-Watcharaporn <138270421+Watcharaporn-Aiamlamai@users.noreply.github.com>
2024-08-17 22:16:39 +0700

    Revert "no message"
    This reverts commit 96fc2a4c2c0b150fa67c6a667de478e6c34655b1.

---
+++ README.md
@@ -0,0 +1,1 @@
+[Test](http://nuttaint.github.io/index.html) | [Demo_Re](http://nuttaint.github.io/Remake/ipad_pro_11____1.html) | [Demo_old](http://nuttaint.github.io/Prototype/Theme1/ipad_pro_11____1.html) | [Dashboard](http://nuttaint.github.io/Dashboard/user.html)
--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-
-const $force = document.querySelector('#force'); // Replace with the actual ID of the element
-const $touches = document.querySelector('#touches'); // Replace with the actual ID of the element
+$force = document.querySelectorAll('#force')[0];
+const $touches = document.querySelectorAll('#touches')[0];
 const fabricCanvas = new fabric.Canvas('canvas', { isDrawingMode: false });
-const currentPageName = window.location.pathname.split('/').pop();
 fabricCanvas.setBackgroundImage('', fabricCanvas.renderAll.bind(fabricCanvas));
-let lineCount = 1; // Initialize the line count
-let rotationAngle = 0;
-let altitudeAngle = 0;
-let azimuthAngle =  0;
-var user = localStorage.getItem('username');
-localStorage.setItem('beforeX', 0);
-localStorage.setItem('beforeY', 0);
-localStorage.setItem('currentX', 0);
-localStorage.setItem('currentY', 0);
 
-console.log('user:', user );
 let lineWidth = 0 ;
 let isMousedown = false;
 let points = [];
 
-let timeCounter = 0;    // Initialize the timeCounter
 fabricCanvas.width = window.innerWidth * 2;
 fabricCanvas.height = window.innerHeight * 2;
 
@@ -31,13 +18,6 @@ fabricCanvas.freeDrawingBrush.color = 'black';
 fabricCanvas.freeDrawingBrush.width = 7;
 
 fabricCanvas.isDrawingMode = !fabricCanvas.isDrawingMode;
-const currentPageURL = window.location.href;
-function euclidean_distance(x1, y1, x2, y2) {
-  const squared_distance = (x2 - x1) ** 2 + (y2 - y1) ** 2;
-  const distance = Math.sqrt(squared_distance);
-  return distance;
-}
-
 
 function drawOnCanvas(points) {
   for (let i = 1; i < points.length; i++) {
@@ -48,15 +28,8 @@ function drawOnCanvas(points) {
 }
 
 fabricCanvas.on('mouse:down', function (e) {
-  console.log('Mouse down event triggered');
   isMousedown = true;
-  points.push({ x: e.e.pageX * 2, y: e.e.pageY * 2, lineWidth });
-
-  // Update localStorage values
-  localStorage.setItem('beforeX', localStorage.getItem('currentX'));
-  localStorage.setItem('beforeY', localStorage.getItem('currentY'));
-  localStorage.setItem('currentX', e.e.pageX * 2);
-  localStorage.setItem('currentY', e.e.pageY * 2);
+  points.push({ x: e.e.pageX * 2, y: e.e.pageY * 2 , lineWidth });
 });
 
 fabricCanvas.on('touch:gesture', function (e) {
@@ -95,6 +68,7 @@ for (const ev of ['touchmove', 'mousemove']) {
     // smoothen line width
     lineWidth = Math.log(pressure + 1) * 40 * 0.2 + lineWidth * 0.8;
     points.push({ x, y , lineWidth});
+
     drawOnCanvas(points);
 
     requestIdleCallback(() => {
@@ -125,24 +99,9 @@ fabricCanvas.on('mouse:move', function (e) {
   const x = e.e.pageX * 2;
   const y = e.e.pageY * 2;
 
-  points.push({ x, y, lineWidth });
-
-  // Update localStorage values
-  localStorage.setItem('beforeX', localStorage.getItem('currentX'));
-  localStorage.setItem('beforeY', localStorage.getItem('currentY'));
-  localStorage.setItem('currentX', x);
-  localStorage.setItem('currentY', y);
-
-  
-  // Calculate and log Euclidean distance
-  const prevX = localStorage.getItem('beforeX');
-  const prevY = localStorage.getItem('beforeY');
-  const distance = euclidean_distance(prevX, prevY, x, y);
-  console.log('Euclidean distance:', distance);
-
+  points.push({ x, y , lineWidth});
   drawOnCanvas(points);
 });
-
 
 fabricCanvas.on('touch:drag', function (e) {
   if (!isMousedown) return
@@ -191,9 +150,9 @@ fabricCanvas.on('mouse:up', function (e) {
     points = [];
     sendDataToServer(numTouches); // Call the sendDataToServer function with the number of touches
     resetStrokeHistory(); // Reset the stroke history after sending data
-    lineCount++; // Increment the line count
-    timeCounter = 0;
   });
+
+
 });
 function drawLine(start, end) {
   fabricCanvas.getContext().beginPath();
@@ -211,36 +170,17 @@ function resetStrokeHistory() {
   strokeHistory.splice(0, strokeHistory.length);
 }
 
-function sendDataToServer(numTouches) {
-  const timestamp = Date.now();
-  const dateObj = new Date(timestamp);
-  const formattedTimestamp = dateObj.toISOString();
 
-  // Calculate Euclidean distance using the most recent coordinates
-  const prevX = localStorage.getItem('beforeX');
-  const prevY = localStorage.getItem('beforeY');
-  const currentX = localStorage.getItem('currentX');
-  const currentY = localStorage.getItem('currentY');
-  const distance = euclidean_distance(prevX, prevY, currentX, currentY);
-  const pressure = points.length > 0 ? points[points.length - 1].lineWidth : 0;
+function sendDataToServer(numTouches) {
   // Convert the points object into an array of objects with additional touch parameters
   const touchDataArrayWithParameters = strokeHistory.flat().map(point => ({
     ...point,
-    rotationAngle: rotationAngle ||  0,
-    altitudeAngle: altitudeAngle || 0,
-    azimuthAngle:  azimuthAngle || 0,
-    currentPageName: currentPageName,
-    lineCount: lineCount,
-    timestamp: formattedTimestamp,
-    user: user,
-    distance: distance || 0, // Include the Euclidean distance
-    force: pressure, // Include the force value
-    timeCounter: timeCounter++  ,    // Initialize the timeCounter
+    rotationAngle: 0,
+    altitudeAngle: 0,
+    azimuthAngle: 0,
   }));
-  
-  console.log('Stroke history from canvas with parameters:', touchDataArrayWithParameters );
-  console.log('Number of touches:', numTouches, currentPageName,user );
-  
+  console.log('Stroke history from canvas with parameters:', touchDataArrayWithParameters);
+  console.log('Number of touches:', numTouches);
   // Replace the URL with the server endpoint where you want to send the data
   fetch('https://k0c9lchx-3000.asse.devtunnels.ms/api/pencil', {
     method: 'POST',
@@ -249,13 +189,9 @@ function sendDataToServer(numTouches) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(touchDataArrayWithParameters), // Send the entire stroke history with parameters to the server
-  }
-  )
-  
+  })
     .then((response) => {
       console.log('Data sent to the server');
-      timeCounter = 0;
-
     })
     .catch((error) => {
       console.error('Error sending data to the server:', error);
