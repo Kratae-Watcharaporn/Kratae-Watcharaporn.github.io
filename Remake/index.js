e301c75f6133c1ca5aa510f9fcc145856a19e72b
kratae-Watcharaporn <138270421+Watcharaporn-Aiamlamai@users.noreply.github.com>
2024-08-17 22:34:21 +0700

    no message

--- Remake/index.js
+++ Remake/index.js
@@ -1,4 +1,3 @@
-
 const $force = document.querySelector('#force'); // Replace with the actual ID of the element
 const $touches = document.querySelector('#touches'); // Replace with the actual ID of the element
 const fabricCanvas = new fabric.Canvas('canvas', { isDrawingMode: false });
@@ -38,7 +37,6 @@ function euclidean_distance(x1, y1, x2, y2) {
   return distance;
 }
 
-
 function drawOnCanvas(points) {
   for (let i = 1; i < points.length; i++) {
     const startPoint = points[i - 1];
@@ -47,38 +45,26 @@ function drawOnCanvas(points) {
   }
 }
 
-// fabricCanvas.on('mouse:down', function (e) {
-//   console.log('Mouse down event triggered');
-//   isMousedown = true;
-//   points.push({ x: e.e.pageX * 2, y: e.e.pageY * 2, lineWidth });
+fabricCanvas.on('touch:start', function (e) {
+  console.log('Touch start event triggered');
+  isMousedown = true;
+  const touch = e.e.touches[0];
+  points.push({ x: touch.pageX * 2, y: touch.pageY * 2, lineWidth });
 
-//   // Update localStorage values
-//   localStorage.setItem('beforeX', localStorage.getItem('currentX'));
-//   localStorage.setItem('beforeY', localStorage.getItem('currentY'));
-//   localStorage.setItem('currentX', e.e.pageX * 2);
-//   localStorage.setItem('currentY', e.e.pageY * 2);
-// });
+  // Update localStorage values
+  localStorage.setItem('beforeX', localStorage.getItem('currentX'));
+  localStorage.setItem('beforeY', localStorage.getItem('currentY'));
+  localStorage.setItem('currentX', touch.pageX * 2);
+  localStorage.setItem('currentY', touch.pageY * 2);
+});
 
 fabricCanvas.on('touch:gesture', function (e) {
   isMousedown = true;
   const touch = e.e.touches[0];
   points.push({ x: touch.pageX * 2, y: touch.pageY * 2 , lineWidth});
-
-    // Update localStorage values
-  localStorage.setItem('beforeX', localStorage.getItem('currentX'));
-  localStorage.setItem('beforeY', localStorage.getItem('currentY'));
-  localStorage.setItem('currentX', e.e.pageX * 2);
-  localStorage.setItem('currentY', e.e.pageY * 2);
 });
 
-function countTouches(array) {
-  let count = 0;
-  for (const _ of array) {
-    count++;
-  }
-  return count;
-}
-for (const ev of ['touchmove', 'mousemove']) {
+for (const ev of ['touchmove']) {
   canvas.addEventListener(ev, function (e) {
     if (!isMousedown) return;
     e.preventDefault();
@@ -106,93 +92,63 @@ for (const ev of ['touchmove', 'mousemove']) {
     requestIdleCallback(() => {
       $force.textContent = 'force = ' + pressure;
 
-      if (e.touches) {
-        const touch = e.touches[0];
-        if (touch) {
-          // Log the touch parameters to the console
-          console.log('Touch parameters:', {
-            rotationAngle: touch.rotationAngle,
-            altitudeAngle: touch.altitudeAngle,
-            azimuthAngle: touch.azimuthAngle,
-          });
-        } else {
-          console.log('No touch parameters available.');
-        }
+      const touch = e.touches[0];
+      if (touch) {
+        console.log('Touch parameters:', {
+          rotationAngle: touch.rotationAngle,
+          altitudeAngle: touch.altitudeAngle,
+          azimuthAngle: touch.azimuthAngle,
+        });
       } else {
-        console.log('Not a touch event.');
+        console.log('No touch parameters available.');
       }
     });
   });
 }
 
-fabricCanvas.on('mouse:move', function (e) {
-  if (!isMousedown) return;
-
-  const x = e.e.pageX * 2;
-  const y = e.e.pageY * 2;
-
-  points.push({ x, y, lineWidth });
-
-  // Update localStorage values
-  localStorage.setItem('beforeX', localStorage.getItem('currentX'));
-  localStorage.setItem('beforeY', localStorage.getItem('currentY'));
-  localStorage.setItem('currentX', x);
-  localStorage.setItem('currentY', y);
-
-  
-  // Calculate and log Euclidean distance
-  const prevX = localStorage.getItem('beforeX');
-  const prevY = localStorage.getItem('beforeY');
-  const distance = euclidean_distance(prevX, prevY, x, y);
-  console.log('Euclidean distance:', distance);
-
-  drawOnCanvas(points);
-});
-
-
 fabricCanvas.on('touch:drag', function (e) {
-  if (!isMousedown) return
-  e.preventDefault()
+  if (!isMousedown) return;
+  e.preventDefault();
 
-  let pressure = 0.1
-  let x, y
+  let pressure = 0.1;
+  let x, y;
   if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
     if (e.touches[0]["force"] > 0) {
-      pressure = e.touches[0]["force"]
+      pressure = e.touches[0]["force"];
     }
-    x = e.touches[0].pageX * 2
-    y = e.touches[0].pageY * 2
+    x = e.touches[0].pageX * 2;
+    y = e.touches[0].pageY * 2;
   } else {
-    pressure = 1.0
-    x = e.pageX * 2
-    y = e.pageY * 2
+    pressure = 1.0;
+    x = e.pageX * 2;
+    y = e.pageY * 2;
   }
 
   // smoothen line width
-  lineWidth = (Math.log(pressure + 1) * 40 * 0.2 + lineWidth * 0.8)
-  points.push({ x, y , lineWidth})
+  lineWidth = Math.log(pressure + 1) * 40 * 0.2 + lineWidth * 0.8;
+  points.push({ x, y , lineWidth });
 
   drawOnCanvas(points);
 
   requestIdleCallback(() => {
-    $force.textContent = 'force = ' + pressure
+    $force.textContent = 'force = ' + pressure;
 
-    const touch = e.touches ? e.touches[0] : null
+    const touch = e.touches ? e.touches[0] : null;
     if (touch) {
       $touches.innerHTML = `
         rotationAngle = ${touch.rotationAngle} <br/>
         altitudeAngle = ${touch.altitudeAngle} <br/>
         azimuthAngle = ${touch.azimuthAngle} <br/>
-      `
+      `;
     }
-  })
-})
+  });
+});
 
-fabricCanvas.on('mouse:up', function (e) {
+fabricCanvas.on('touch:end', function (e) {
   isMousedown = false;
 
   requestIdleCallback(function () {
-    const numTouches = countTouches(points); // Count the number of touches
+    const numTouches = points.length; // Count the number of touches
     strokeHistory.push([...points]); // Add the current touch data array to the strokeHistory
     points = [];
     sendDataToServer(numTouches); // Call the sendDataToServer function with the number of touches
@@ -201,6 +157,7 @@ fabricCanvas.on('mouse:up', function (e) {
     timeCounter = 0;
   });
 });
+
 function drawLine(start, end) {
   fabricCanvas.getContext().beginPath();
   fabricCanvas.getContext().moveTo(start.x, start.y);
@@ -229,12 +186,12 @@ function sendDataToServer(numTouches) {
   const currentY = localStorage.getItem('currentY');
   const distance = euclidean_distance(prevX, prevY, currentX, currentY);
   const pressure = points.length > 0 ? points[points.length - 1].lineWidth : 0;
-  // Convert the points object into an array of objects with additional touch parameters
+
   const touchDataArrayWithParameters = strokeHistory.flat().map(point => ({
     ...point,
-    rotationAngle: rotationAngle ||  0,
+    rotationAngle: rotationAngle || 0,
     altitudeAngle: altitudeAngle || 0,
-    azimuthAngle:  azimuthAngle || 0,
+    azimuthAngle: azimuthAngle || 0,
     currentPageName: currentPageName,
     lineCount: lineCount,
     timestamp: formattedTimestamp,
@@ -244,8 +201,8 @@ function sendDataToServer(numTouches) {
     timeCounter: timeCounter++  ,    // Initialize the timeCounter
   }));
   
-  console.log('Stroke history from canvas with parameters:', touchDataArrayWithParameters );
-  console.log('Number of touches:', numTouches, currentPageName,user );
+  console.log('Stroke history from canvas with parameters:', touchDataArrayWithParameters);
+  console.log('Number of touches:', numTouches, currentPageName, user);
   
   // Replace the URL with the server endpoint where you want to send the data
   fetch('https://k0c9lchx-3000.asse.devtunnels.ms/api/pencil', {
@@ -255,13 +212,11 @@ function sendDataToServer(numTouches) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(touchDataArrayWithParameters), // Send the entire stroke history with parameters to the server
-  }
-  )
+  })
   
     .then((response) => {
       console.log('Data sent to the server');
       timeCounter = 0;
-
     })
     .catch((error) => {
       console.error('Error sending data to the server:', error);
