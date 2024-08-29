508575ea2b5856d4cd7a96a3b3083f15cd97e835
kratae-Watcharaporn <138270421+Watcharaporn-Aiamlamai@users.noreply.github.com>
2024-08-29 20:37:56 +0700

    no message

--- Remake/index.js
+++ Remake/index.js
@@ -212,14 +212,14 @@ function sendDataToServer(numTouches) {
     direction: point.direction,
   }));
 
-  console.log("Data to server:", {
+  // ตรวจสอบค่าที่จะส่งไปยังเซิร์ฟเวอร์
+    console.log("Data to be sent to server:", {
     totalDrawingTime,
     averageSpeed,
     totalDeviation,
     touchDataArrayWithParameters,
   });
 
-  // ส่งข้อมูลไปยังเซิร์ฟเวอร์
   fetch('https://k0c9lchx-3000.asse.devtunnels.ms/api/pencil', {
     method: 'POST',
     mode: 'cors',
@@ -233,9 +233,9 @@ function sendDataToServer(numTouches) {
       touchDataArrayWithParameters
     }),
   })
-  .then(response => {
-    console.log('Data sent to the server');
-    timeCounter = 0;
+  .then(response => response.json())  // ตรวจสอบการตอบกลับของเซิร์ฟเวอร์
+  .then(data => {
+    console.log('Server response:', data);  // ตรวจสอบข้อมูลที่ตอบกลับมาจากเซิร์ฟเวอร์
   })
   .catch(error => {
     console.error('Error sending data to the server:', error);
